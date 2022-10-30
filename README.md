49f011a03c23c6b4a2512e04f777fe433aba2027
Zaria Chen Shui <zariachenshui@gmail.com>
2022-10-30 16:19:58 -0500

    Edited styles.css and scripts.js and inserted six images

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# info2180-project1
-
 # INFO 2180 Project 1
 
 # This is Project 1 for Zaria Chen Shui
--- scripts.js
+++ scripts.js
@@ -1,1 +1,53 @@
 /* Add your JavaScript to this file */
+
+window.onload = function () {
+    var emailForm = document.getElementsByTagName("form")[0];
+
+    emailForm.addEventListener('submit', function (element) {
+        element.preventDefault();
+
+        // Removes any previous messages so there isn't an increasing stack of messages after multiple clicks
+        clearMessage();
+
+        var validationFailed = false;
+
+        var userEmail = document.querySelector("#email");
+
+        if (!isValidEmail(userEmail.value.trim()) || isEmpty(userEmail.value.trim())) {
+            validationFailed = true;
+            displayMessage(true);
+        } else {
+            displayMessage(false, userEmail.value.trim());
+        }
+    });
+};
+
+function isValidEmail(email) {
+    return /^\S+@\S+\.\S+$/.test(email);
+}
+
+function isEmpty(something) {
+    if (something.length == 0) {
+        return true;
+    }
+    return false;
+}
+
+function displayMessage(isInvalid, userEmail = '') {
+    if (isInvalid) {
+        message = `Please enter a valid email address.`;
+    } else {
+        message = `Thank you! Your email address ${userEmail} has been added to our mailing list!`;
+    }
+
+    var messageText = document.createTextNode(message);
+    var messageBox = document.createElement('p');
+    messageBox.appendChild(messageText);
+    document.getElementsByClassName('message')[0].appendChild(messageBox);
+}
+
+function clearMessage() {
+    pastMessages = document.getElementsByClassName('message')[0];
+    pastMessages.innerHTML = "";
+}
+
